e38e71c9ab83aa27acc1a1875bdcd400391f0bc4
Avinash Kathiravan <avinashkathiravan@gmail.com>
2023-01-08 22:11:49 +0530

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,26 @@
+import Alert from './Alert'
+import BigSideBar from './BigSideBar'
+import ChartsContainer from './ChartsContainer'
+import FormRow from './FormRow'
+import FormRowSelect from './FormRowSelect'
+import JobsContainer from './JobsContainer'
+import Loading from './Loading'
+import Logo from './Logo'
+import Navbar from './Navbar'
+import SearchContainer from './SearchContainer'
+import SmallSideBar from './SmallSideBar'
+import StatsContainer from './StatsContainer'
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  BigSideBar,
+  SmallSideBar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  StatsContainer,
+  ChartsContainer,
+  Loading,
+}
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+          Track your jobs like a pro with <span>Jobify</span>
+          </h1>
+          <p>
+          Are you still tracking jobs in Excel or Google Sheets? 
+          Track and manage your job applications and interviews all in one place.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
